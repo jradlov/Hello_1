03160ba2f701020c04c2d1b8f1578ebcbdea5c1a
Jaime Radulovich <jradlov@gmail.com>
2018-07-10 08:40:13 -0700

    Add project files.

---
+++ Hello_1/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Hello_1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button btn = FindViewById<Button>(Resource.Id.button1);
+            TextView txtView = FindViewById<TextView>(Resource.Id.textView1);
+            EditText txtEdit = FindViewById<EditText>(Resource.Id.editText1);
+
+            btn.Click += delegate {
+                txtView.Text = txtEdit.Text;
+                txtEdit.Text = "";
+            };
+        }
+    }
+}
+
